0f90f0fb17ae25133e6894a850fac53f0dfc5874
Breven <breven95@comcast.net>
2015-11-07 21:37:44 -0500

    Added initial authentication procedures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :tasks
 	has_many :answers
+	has_secure_password
 	validates :userName, presence: true
 	validates :firstName, presence: true
 	validates :lastName, presence: true
